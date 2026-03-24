c88d4e0d3a939541237cee3b5ec672e9986c5bfb
mertokumus01 <okumus01mert@hotmail.com>
2026-03-24 15:01:15 +0300

    Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Countries } from "./countries/countries";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Countries],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/countries/countries.ts
+++ src/app/countries/countries.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { CountryService } from '../country.service';
+import { CountryModel } from '../models/country.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-countries',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './countries.html',
   styleUrl: './countries.css',
 })
-export class Countries {}
+export class Countries {
+
+  private countryService = inject(CountryService);
+
+  countryList : Observable<CountryModel[]> = this.countryService.getCountry();
+
+  consoleLog(){
+    console.log("DEneme");
+    console.log(this.countryList);
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.consoleLog();
+  }
+
+}
