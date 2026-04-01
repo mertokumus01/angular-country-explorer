213e522b1ec5cf42f0934ec67036f2d6a53c9ac3
mertokumus01 <okumus01mert@hotmail.com>
2026-04-01 10:35:27 +0300

    Last Update

--- src/app/countries/countries.ts
+++ src/app/countries/countries.ts
@@ -1,15 +1,16 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal, OnInit } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
 import { CountryService } from '../country.service';
 import { CountryModel } from '../models/country.model';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-countries',
-  imports: [FormsModule],
+  imports: [FormsModule,KeyValuePipe],
   templateUrl: './countries.html',
   styleUrl: './countries.css',
 })
-export class Countries {
+export class Countries implements OnInit {
   private countryService = inject(CountryService);
 
   countryName = signal('');
@@ -20,12 +21,16 @@ export class Countries {
   }
 
   search() {
+    // Signal'dan kullanıcı tarafından girilen ülke adını al
     const name = this.countryName();
 
+    // Eğer arama kutusunda metin girildiyse
     if (name.length > 0) {
+      // Spesifik ülkeyi ara ve sonucu countryList signal'ına aktar
       this.countryService.getCountryByName(name)
         .subscribe(res => this.countryList.set(res));
     } else {
+      // Arama kutusunda hiç metin yoksa tüm ülkeleri getir
       this.countryService.getCountry()
         .subscribe(res => this.countryList.set(res));
     }
--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { CountryModel } from './models/country.model';
 
 @Injectable({
@@ -10,19 +10,29 @@ export class CountryService {
 
   private http = inject(HttpClient);
 
+
   private apiUrl = "https://restcountries.com/v3.1/all?fields=name,capital,currencies";
 
   getCountry(): Observable<CountryModel[]>{
-    return this.http.get<CountryModel[]>(this.apiUrl);
+    return this.http.get<CountryModel[]>(this.apiUrl).pipe(
+      catchError((error) => {
+        console.error('API error : ',error);
+        return of([]); // Return an empty array in case of error
+      })
+    );
   }
 
   getCountryByName(countryName: string): Observable<CountryModel[]> {
-  return this.getCountry().pipe(
-    map((countries: CountryModel[]) =>
-      countries.filter(c =>
-        c.name.common.toLowerCase().includes(countryName.toLowerCase())
-      )
-    )
-  );
-}
+    return this.getCountry().pipe(
+      map((countries: CountryModel[]) =>
+        countries.filter(c =>
+          c.name.common.toLowerCase().includes(countryName.toLowerCase())
+        )
+      ),
+      catchError(error =>{
+        console.error('Error filtering countries: ', error);
+        return of([]); // Return an empty array in case of error
+      })
+    );
+  }
 }
